e8a5dd527cd8cf406cd9be8f5f666ffa0b35b304
KatjaArndt <80687991+KatjaArndt@users.noreply.github.com>
2021-06-10 17:46:49 +0200

    deleted redirects, empty index.html and added forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,9 +1,25 @@
 import React from "react";
+import WeatherIcon from "./WeatherIcon.js";
+
 //import "./WeatherForecast.css";
+
 export default function WeatherForecast() {
   return (
-    <div className="Forecast">
-      <div className="weather-forecast forecast">Forecast</div>
+    <div className="WeatherForecast">
+      <div className="weather-forecast forecast">
+      <div class="row">
+      <div class="col">
+        <div class="weather-forecast-day">Mon</div>
+        <WeatherIcon code="01d"/>
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> 20° </span>
+          <span class="weather-forecast-temperature-min"> 10° </span>
+        </div>
+      </div>
+      </div>
+      
+      </div>
     </div>
   );
 }
