4ef76a651e2112e2fcabefab7552d4b1c272e217
KatjaArndt <80687991+KatjaArndt@users.noreply.github.com>
2021-06-13 22:24:15 +0200

    refactoring

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -2,16 +2,16 @@ import React from "react";
 import WeatherIcon from "./WeatherIcon.js";
 
 export default function WeatherForecastDay(props) {
-function maxTemperature(){
+  function maxTemperature() {
     let temperature = Math.round(props.data.temp.max);
     return `${temperature}°`;
-}
-function minTemperature(){
+  }
+  function minTemperature() {
     let temperature = Math.round(props.data.temp.min);
     return `${temperature}°`;
-}
+  }
 
-function day() {
+  function day() {
     let date = new Date(props.data.dt * 1000);
     let day = date.getDay();
 
@@ -19,16 +19,20 @@ function day() {
 
     return days[day];
   }
-    return ( 
-        <div className="col">
-            <div className="weather-forecast-day">
-                {day()}
-            </div>
-            <WeatherIcon code={props.data.weather[0].icon} size={30}/>
-            <div className="weather-forecast-temperatures">
-                <span className="weather-forecast-temperature-max"> {maxTemperature()} </span>
-                <span className="weather-forecast-temperature-min"> {minTemperature()} </span>
-            </div> 
-        </div>
-   );
+  return (
+    <div className="col">
+      <div className="weather-forecast-day">{day()}</div>
+      <WeatherIcon code={props.data.weather[0].icon} size={30} />
+      <div className="weather-forecast-temperatures">
+        <span className="weather-forecast-temperature-max">
+          {" "}
+          {maxTemperature()}{" "}
+        </span>
+        <span className="weather-forecast-temperature-min">
+          {" "}
+          {minTemperature()}{" "}
+        </span>
+      </div>
+    </div>
+  );
 }
--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,11 +1,10 @@
 import React from "react";
 
-export default function WeatherTemperature (props){
-           return( 
-        <span>
-            <span className="current-temperature">{props.celsius}</span>
-            <span className="units">°C </span>  
-        </span>
-    ); 
-  
+export default function WeatherTemperature(props) {
+  return (
+    <span>
+      <span className="current-temperature">{props.celsius}</span>
+      <span className="units">°C </span>
+    </span>
+  );
 }
